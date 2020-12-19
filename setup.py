463d179711518aece36ca15dad28f8d3956fc311
OneOvano <72787569+OneOvano@users.noreply.github.com>
2020-12-19 18:37:11 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lias',
+    version='0.1',
+    packages=find_packages(),
+    url='https://github.com/OneOvano/LIAS',
+    license='MIT',
+    author='vano',
+    author_email='iv.shaposhnikov@gmail.com',
+    description='Lines Identification in Astrophysical Spectra'
+)
