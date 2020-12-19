e2da35fa837d7c6efffff6ba905694cd3ebd7233
OneOvano <72787569+OneOvano@users.noreply.github.com>
2020-12-20 02:49:20 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='lias',
+    version='0.1',
+    packages=['lias'],
+    url='https://github.com/OneOvano/LIAS',
+    license='MIT',
+    install_requires=(
+        'numpy>=1.18.0'
+        'pandas>=1.1.0'
+        'urllib>=1.7'
+        'scipy>=1.5.0'
+        'lxml>=4.5.0'
+        'matplotlib>=3.0.0'
+        'PyQt5'
+    ),
+    test_suite='test',
+    author='Vano',
+    author_email='iv.shaposhnikov@gmail.com',
+    description='Lines Identification in Astrophysical Spectra'
+)
---
+++ test.py
@@ -0,0 +1,77 @@
+import lias
+
+import unittest
+import numpy as np
+
+
+EPS = 1e-3
+
+
+class test_spectra(unittest.TestCase):
+    """
+    Testing of parser and Spectra methods
+    """
+
+    def test_load(self):
+        o_iii = lias._load('O III', 6500, 6600)
+        o_iii = o_iii.to_dict('list')
+        target = {
+            'Wavelength, Å': [6507.55],
+            'A_ki': [39800000.0],
+            'Lower Level Term': ['1P'],
+            'Lower Level J': [1.0],
+            'Upper Level Term': ['1P°'],
+            'Upper Level J': [1.0],
+            'Type': ['']
+        }
+        assert o_iii == target
+
+    def test_spec(self):
+        spectrum = np.array([[6500., 1.0], [6660., 1.0]])
+        sp = lias.Spectra(spectrum, 'O III')
+        target = sp.Object_Spectrum - spectrum
+        assert (sp == target).all
+
+
+class test_tools(unittest.TestCase):
+    """
+    Testing of the tools for working with lines
+    """
+
+    def test_calc_cont(self):
+        x = [6500, 6600]
+        y = [1., 1.]
+        wln = 6550
+        target = 1.
+        assert (lias.Tools.calc_cont(x, y, wln, 1) - target) < EPS
+
+    def test_masc(self):
+        x = np.array([1., 2., 3.])
+        y = np.array([0., 1., 0.])
+        target = 2.
+        assert (lias.Tools.calc_masc(x, y) - target) < EPS
+
+    def test_intense(self):
+        x = np.linspace(0, 1, 100)
+        y = x**3
+        cnt = np.linspace(0, 0, 100)
+        target = 0.25
+        assert np.abs(lias.Tools.calc_int(x, y, cnt)/target-1) < EPS
+
+    def test_gauss(self):
+        x = np.linspace(-1, 1, 100)
+        target = (1., 0., 0.5)
+        y = lias.Tools.gauss_function(
+            x,
+            target[0],
+            target[1],
+            target[2]
+        )
+        cnt = 0
+        output = lias.Tools.fit_gauss(x, y, cnt)
+        accuracy = np.abs(np.array(output - target))
+        assert np.max(accuracy) < EPS
+
+
+if __name__ == '__main__':
+    unittest.main()
