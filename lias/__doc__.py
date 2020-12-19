2d9941f6efd295aad390df30428c4414da49e3a5
OneOvano <72787569+OneOvano@users.noreply.github.com>
2020-12-19 20:27:56 +0300

    Delete __doc__.py

--- lias/__doc__.py
+++
@@ -1,6 +0,0 @@
-"""
-    >>>>>>> LIAS == Lines Indentification in Astrophysical Spectra <<<<<<<
-    Prototype of a software package for the identification of atomic lines 
-in the astrophysical objects spectra (with using NIST Atomic Spectra Database)
-"""
-
