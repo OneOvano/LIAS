9952b2871d4c6f1a525c929ec27f86f3c3fd15d3
OneOvano <72787569+OneOvano@users.noreply.github.com>
2020-12-20 02:52:05 +0300

    Update __init__.py

--- lias/__init__.py
+++ lias/__init__.py
@@ -1,10 +1,6 @@
-"""
-||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-|||||||||||||| LIAS == Lines Indentification in Astrophysical Spectra ||||||||||||||
-|||||||Prototype of a software package for the identification of atomic lines|||||||
-|||in the astrophysical objects spectra (with using NIST Atomic Spectra Database)|||
-||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-
+"""       LIAS == Lines Indentification in Astrophysical Spectra
+Prototype of a software package for the identification of atomic lines
+in the astrophysical objects spectra (with using NIST Atomic Spectra Database)
 """
 
 import lias.Tools as Tools
