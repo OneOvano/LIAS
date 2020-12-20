f4d8ab5f2fd0f1c23d33826a44b284e0f63be3c0
OneOvano <72787569+OneOvano@users.noreply.github.com>
2020-12-20 03:10:05 +0300

    Update __init__.py

--- lias/__init__.py
+++ lias/__init__.py
@@ -3,9 +3,11 @@ Prototype of a software package for the identification of atomic lines
 in the astrophysical objects spectra (with using NIST Atomic Spectra Database)
 """
 
+
 import lias.Tools as Tools
 from lias.Spectra import Spectra, _load
 from lias.DoWork import DoWork
+
 
 __author__ = "Vano"
 __version__ = "0.1"
