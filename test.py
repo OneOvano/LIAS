dd552b2621d2b41724480ddbb33513b4c9691fc0
OneOvano <72787569+OneOvano@users.noreply.github.com>
2020-12-21 13:01:09 +0300

    Update test.py

--- test.py
+++ test.py
@@ -4,9 +4,6 @@ import unittest
 import numpy as np
 
 
-EPS = 1e-3
-
-
 class test_spectra(unittest.TestCase):
     """
     Testing of parser and Spectra methods
@@ -29,9 +26,9 @@ class test_spectra(unittest.TestCase):
     def test_spec(self):
         spectrum = np.array([[6500., 1.0], [6660., 1.0]])
         sp = lias.Spectra(spectrum, 'O III')
-        target = sp.Object_Spectrum - spectrum
-        assert (sp == target).all
-
+        target = sp.Object_Spectrum
+        np.testing.assert_equal(spectrum, target)
+        
 
 class test_tools(unittest.TestCase):
     """
@@ -43,34 +40,29 @@ class test_tools(unittest.TestCase):
         y = [1., 1.]
         wln = 6550
         target = 1.
-        assert (lias.Tools.calc_cont(x, y, wln, 1) - target) < EPS
+        np.testing.assert_allclose(lias.Tools.calc_cont(x, y, wln, 1), target)
 
     def test_masc(self):
         x = np.array([1., 2., 3.])
         y = np.array([0., 1., 0.])
         target = 2.
-        assert (lias.Tools.calc_masc(x, y) - target) < EPS
+        np.testing.assert_allclose(lias.Tools.calc_masc(x, y), target)
 
     def test_intense(self):
         x = np.linspace(0, 1, 100)
         y = x**3
         cnt = np.linspace(0, 0, 100)
         target = 0.25
-        assert np.abs(lias.Tools.calc_int(x, y, cnt)/target-1) < EPS
+        np.testing.assert_allclose(lias.Tools.calc_int(x, y, cnt), target)
 
     def test_gauss(self):
         x = np.linspace(-1, 1, 100)
         target = (1., 0., 0.5)
-        y = lias.Tools.gauss_function(
-            x,
-            target[0],
-            target[1],
-            target[2]
-        )
+        y = lias.Tools.gauss_function(x, *target)
         cnt = 0
         output = lias.Tools.fit_gauss(x, y, cnt)
         accuracy = np.abs(np.array(output - target))
-        assert np.max(accuracy) < EPS
+        np.testing.assert_allclose(accuracy, target)
 
 
 if __name__ == '__main__':
